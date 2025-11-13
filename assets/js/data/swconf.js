f59e8215ad79ff9f187f986875ee038fbfddd7a4
jerryko570 <jerryko570@users.noreply.github.com>
2025-11-13 17:06:44 +0000

    deploy: 1448ba3146e0643ff6a4b9d32951975274ef34ef

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1763053602',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
